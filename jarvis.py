5e4051a916cf230343031c7923206a1a018ee669
Bidesh <bideshmalofficial@gmail.com>
2023-04-16 02:05:09 +0530

    Initial commit

---
+++ jarvis.py
@@ -0,0 +1,120 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+import pyaudio
+import webbrowser
+import os
+import smtplib
+engine = pyttsx3.init('sapi5')
+voices= engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voice',voices[1].id)
+
+
+rate = engine.getProperty('rate')
+
+print(rate)
+
+engine.setProperty('rate',150)
+
+ 
+newVoiceRate = 50
+ 
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    #print(hour)
+    
+    if hour>=4 and hour<=12:
+        speak("Good Morning")
+    
+    elif hour>=12 and hour<18:
+        speak("Good afternoon")
+    
+    elif hour>=18 and hour<=20:
+        speak("Good evening")
+    
+    else:
+        speak("Good night")
+    
+    speak("I am Jarvis SUMANA MADAM. HAPPY BIRTHDAY TO U, MAY ALL THE PROSPERITY AND BEST THINGS COME TO U. Please tell me how may I help you")
+    
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+ 
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('asskicker680@gmail.com', 'ahbajb5839@')
+    server.sendmail('asskicker680@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    speak("OV created me")
+    while True:
+    # if 1:
+        query = takeCommand().lower() #Converting user query into lower case
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:  #if wikipedia found in the query then this block will be executed
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2) 
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+            
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'play music' in query:
+            music_dir = 'C:\music'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        elif 'open code' in query:
+            codePath = "C:\\Users\\KIIT\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+        
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com") 
+
+        elif 'email to videsh' in query:
+            try:
+                speak("What should I send?")
+                content = takeCommand()
+                to = "bideshmalofficial@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend. I am not able to send this email at this moment")    
+#if __name__=="__main__":
+    #speak("I love you shrestha ")
+    
